3ae53e71751faf58c16371ab65cee8cbb3898882
Hans Durchholz <hansied678@gmail.com>
2020-12-07 07:01:32 -0600

    Day 7 MINOR REFACTOR: cleaned up code

--- day7/day7.py
+++ day7/day7.py
@@ -1,34 +1,40 @@
-import sys
 import re
 
 IN_FILE = 'day7-input.txt'
+
+BAG_TYPE = 'shiny gold'
+COLOR_REGEX = r'(.+?) bags'
+CONTAINS_REGEX = r'(\d+) (.+?) bag'
 
 
 def get_input(in_file: str) -> dict:
     graph = {}
     with open(in_file, 'r') as f:
         for line in f.readlines():
-            color = re.match(r'(.+?) bags', line).group(1)
-            contains = re.findall(r'(\d+) (.+?) bag', line)
+            color = re.match(COLOR_REGEX, line).group(1)
+            contains = re.findall(CONTAINS_REGEX, line)
             graph[color] = contains
     return graph
 
 
-def has_shiny(color, graph):
-    if color == 'shiny gold':
+def has_bag_type(bag_type: str, color: str, graph: dict) -> bool:
+    if color == bag_type:
         return True
-    return any(has_shiny(c, graph) for _, c in graph[color])
+    return any(has_bag_type(bag_type, c, graph) for _, c in graph[color])
 
 
-def count(bag_type, graph):
+def count(bag_type: str, graph: dict) -> int:
     return 1 + sum(int(n)*count(c, graph) for n, c in graph[bag_type])
 
 
 def main():
     graph = get_input(IN_FILE)
-    
-    print(sum(has_shiny(c, graph) for c in graph.keys()) - 1)
-    print(count('shiny gold', graph) - 1)
+
+    part1 = sum(has_bag_type(BAG_TYPE, c, graph) for c in graph.keys()) - 1
+    part2 = count(BAG_TYPE, graph) - 1
+
+    print("Part 1:\n\tBags that can hold shiny gold bags:\n\t\t{}".format(part1))
+    print("Part 2:\n\tRequired amount of bags in 1 shiny gold bag:\n\t\t{}".format(part2))
 
 
 if __name__ == '__main__':
